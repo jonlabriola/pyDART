98e6b2ad2006fa43ca308f95aaddfd847b0b0aac
jonlabriola <jonathan.labriola@noaa.gov>
2021-07-29 14:18:43 -0500

    updated file name

--- scripts/gen_sequence.py
+++ scripts/gen_sequence.py
@@ -11,18 +11,19 @@ time = 10
 create_sequence = True
 create_class = False 
 types = ['radar','conventional']
-
-radar_class = '../output/radar_obs_200905160138.pickle'
-conv_class =  '../output/conv_obs_200905160138.pickle'
-
+time = '200905152313'
+#radar_class = '../output/radar_obs_200905160138.pickle'
+#conv_class = '../output/conv_obs_200905160143.pickle' 
+#conv_class = '../output/conv_obs_200905160208.pickle'
+conv_class = '../output/conv_obs_%s.pickle'%time
 #--- Merge Together Radar/Conventional Observations
 conv = pickle.load(open(conv_class,"rb"))
-radar = pickle.load(open(radar_class,"rb"))
-observations = {**radar.obs, **conv.obs} #--- Merge Observation Dictionaries
-
+#radar = pickle.load(open(radar_class,"rb"))
+#observations = {**radar.obs, **conv.obs} #--- Merge Observation Dictionaries
+observations = conv.obs
 
 #--- Create a Sequence Files
-filepath = '../output/obsequence.txt'
+filepath = '../output/ob_seq_%s.out'%time
 obsseq = pydart.sequence.create_sequence(filepath)
 obsseq.add_header(observations)   #--- Make Sequence Header
 obsseq.add_entries(observations)  #--- Make Observation Entries
