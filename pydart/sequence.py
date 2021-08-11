ad4612c774c8e65ebda9cfcc7f8d896a8732db6e
jonlabriola <jonathan.labriola@noaa.gov>
2021-08-11 16:38:01 -0500

    Updated the sequence program to being reading in observations.  Can currently only read in radar information, but will shortly update code to read in conventional observations as well.

--- pydart/sequence.py
+++ pydart/sequence.py
@@ -4,9 +4,20 @@ import numpy as np
 import datetime as dt
 import pydart
 
-
 class create_sequence():
-   def __init__(self,filepath):
+   """
+   This class is used to generate an observation sequence file given observation classes that are generated 
+   via pyDart
+
+   Require Inputs: 
+      filepath - The path of the obs_sequence file
+      observaitions - the observation class
+
+   Output:
+      An ascii text file that emulates obs_sequence files created by DART 
+   """
+
+   def __init__(self,filepath,observations):
       try:
          os.system('rm %s'%filepath)
       except:
@@ -22,6 +33,8 @@ class create_sequence():
       self.obname = None
       self.obcode = None 
 
+      self.add_header(observations)
+      self.add_entries(observations)
  
    def obinfo(self,observations):
       """
@@ -174,7 +187,13 @@ class create_sequence():
                    self.txtfile.write(" %20.14E\n"%ob) #--- JDL One of these includes errors find out which one, and how to treat
                    self.txtfile.write(" %20.14E\n"%ob) #--- JDL One of these includes errors find out which one, and how to treat this
                    self.txtfile.write(" %20.14E\n"%int(platform[-3:])) #--- Platform Number e.g., RADAR_002 = 002
-                   self.txtfile.write(" %20.14E\n"%np.absolute(flag[oindex]))              #--- Quality Control Flag     
+                   #--- JDL This is done to avoid assimilating any observations on the edge boundaries (Results in a DART Error)
+                   #--- This is currently hard coded for the QLCS Case 
+                   if xloc[oindex] < 1000. or xloc[oindex] > 199000. or yloc[oindex] < 1000. or yloc[oindex] > 199000. or zloc[oindex] < 150. or zloc[oindex] >15000:
+                      self.txtfile.write(" %20.14E\n"%np.absolute(99999.))
+                   #--- JDL This is what you would do other wise
+                   else:
+                      self.txtfile.write(" %20.14E\n"%np.absolute(flag[oindex]))              #--- Quality Control Flag     
                    #--- ADDING THREE LOCATION MARKER
                    if self.nob == 1:
                       self.txtfile.write("      %d          %d          %d\n"%(-1,-1,-1))
@@ -183,7 +202,7 @@ class create_sequence():
                    elif obnum == (self.nob - 1):  #--- Final Observation
                       self.txtfile.write("      %d          %d          %d\n"%(obnum,-1,-1))
                    else:                          #--- Every Other Observation
-                      self.txtfile.write("      %d          %d          %d\n"%(obnum,obnum+1,-1))
+                      self.txtfile.write("      %d          %d          %d\n"%(obnum,obnum+2,-1))
                    self.txtfile.write("obdef\n")
                    self.txtfile.write("loc3Dxyz\n")
                    self.txtfile.write("    %20.14f          %20.14f        %20.14f    \n"%(xloc[oindex],yloc[oindex],zloc[oindex])) #--- Obs Location
@@ -215,20 +234,189 @@ class create_sequence():
 
                    obnum += 1    
 
-
-
-
-
-
-
-            #if observation[platform][obname]['obstype'] == 36:
-      #else:
       self.txtfile.close()
 
 
+class read_sequence():
+   """
+   This class is used to read in observation sequence files 
+   """
+   def __init__(self,filepath):
+      self.read_header(filepath)
+      self.read_radObs(filepath)
+
+   def read_header(self,filepath):
+      """
+      The goal of this function is to read an ascii output file and save the relevant information into an observation class
+
+      Required inputs: Filepath
+      """
+      lines = open(filepath)
+      self.obcode = []
+      self.obname = []
+
+      self.copy = []
+      self.qc = []
+
+      self.obindex =[]
+
+      #--- Adding Holders
+      num_ob_types = 999.
+      copy_start = 999.
+      copy_start = 999.
+      qc_start   = 999.
+      qc_stop    = 999.
+
+     
+      for lindex, line in enumerate(lines):
+         if lindex == 2: 
+            #--- Number of Observation Types
+            num_ob_types = int(line)
+
+         elif lindex > 2 and lindex <= 2+num_ob_types:
+            #--- Read the observation  types and there coresponding codes
+            ln = line.split()
+            self.obcode.append(int(ln[0]))
+            self.obname.append(ln[1])
+
+         elif lindex == 3+num_ob_types:
+            #--- Number of Ob copies/QC Flags
+            ln = line.split()
+            self.ncopies = int(ln[1])
+            self.nqc = int(ln[3])
+
+            #--- Updating the boundary limits
+            copy_start = 4+num_ob_types
+            copy_stop = copy_start+self.ncopies
+            qc_start = copy_stop
+            qc_stop = qc_start+self.nqc
+
+         elif lindex == 4+num_ob_types:
+            #--- Read in the number of obs 
+            ln = line.split()
+            self.nob = int(ln[1])  
+
+         elif lindex > copy_start and lindex <= copy_stop:
+            #--- Read in all the observation copies
+            self.copy.append(line)            
+
+         elif lindex > qc_start and lindex <= qc_stop:
+            #--- Read in the QC flags
+            self.qc.append(line)
+         
+         elif lindex > qc_stop:
+            #--- Break Statement to save time
+            if 'OBS' in line:
+               self.obindex.append(lindex)
+            #self.obstart = lindex+1
+            #break
 
 
 
+   def read_radObs(self,filepath):
+      """
+      How to read observation types
+      
+      Should I read by observation type?
+      """
+      lines = open(filepath)
+      test = lines.readlines()
+      plat_index   = len(self.copy) + 1    #--- Observation Platform
+      flag_index   = len(self.copy) + 2    #--- JDL Flag   
+      dartqc_index = len(self.copy) + 3    #--- DART QC Flag
+      loc_index    = len(self.qc) + len(self.copy) + 4 #--- Location of the Observation locations
+      kindex       = len(self.qc) + len(self.copy) + 6 #--- Observation Kind
+
+
+      nobs = 1
+      nplat = 1
+      nobs_radar = []
+      dbz_ob_index = []
+      vr_ob_index = []
+
+      #---Step 1 Get The Number of Observations For Each Radar
+      for index in self.obindex:
+         platform = float(test[plat_index+index].split()[0])
+         if int(test[kindex+index]) == 45:
+            dbz_ob_index.append(index)
+            if platform > nplat:
+               nobs_radar.append(nobs) 
+               nobs = 1
+               nplat = platform
+            else:
+               nobs += 1
+
+         elif int(test[kindex+index]) == 46:
+            vr_ob_index.append(index)
+
+      #--- Add Final Platform
+      nobs_radar.append(nobs)
+
+      #--- Step 2: Create the Radar Dictionary
+      obs = {}
+      print('JDL numplat = ',nobs_radar)
+      for nrdr,nob in enumerate(nobs_radar):
+         plat_name = 'radar_%03d'%(nrdr+1)
+         obs[plat_name]={}
+         #print('Number of obs = ',nobs_radar)
+         #--------------------------#
+         #--- Radar Reflectivity ---#
+         #--------------------------#
+         obs[plat_name]['RADAR_REFLECTIVITY'] = {}
+         obs[plat_name]['RADAR_REFLECTIVITY']['obx']     = np.zeros((nob))
+         obs[plat_name]['RADAR_REFLECTIVITY']['oby']     = np.zeros((nob))
+         obs[plat_name]['RADAR_REFLECTIVITY']['obz']     = np.zeros((nob))
+         obs[plat_name]['RADAR_REFLECTIVITY']['flag']    = np.zeros((nob))  
+         obs[plat_name]['RADAR_REFLECTIVITY']['DART_QC'] = np.zeros((nob))
+         for cindex,copy in enumerate(self.copy):
+            #--- Loop Over the Different Copies
+            obs[plat_name]['RADAR_REFLECTIVITY'][copy] = np.zeros((nob)) 
+            obcount = 0 
+            for oindex in dbz_ob_index:
+               #--- Make sure platform number is the same as radar number
+               if float(test[oindex+plat_index]) == nrdr+1: 
+                  ob_loc = test[oindex+loc_index].split()
+                  obs[plat_name]['RADAR_REFLECTIVITY'][copy][obcount]      = float(test[oindex+cindex+1])
+                  if cindex == 0:
+                     obs[plat_name]['RADAR_REFLECTIVITY']['obx'][obcount]     = float(ob_loc[0])
+                     obs[plat_name]['RADAR_REFLECTIVITY']['oby'][obcount]     = float(ob_loc[1])
+                     obs[plat_name]['RADAR_REFLECTIVITY']['obz'][obcount]     = float(ob_loc[2]) 
+                     obs[plat_name]['RADAR_REFLECTIVITY']['flag'][obcount]    = float(test[oindex+flag_index])
+                     obs[plat_name]['RADAR_REFLECTIVITY']['DART_QC'][obcount] = float(test[oindex+dartqc_index])
+                  obcount += 1
+
+         #--------------------------#
+         #---  Radial Velocity   ---#
+         #--------------------------#
+         obs[plat_name]['DOPPLER_RADIAL_VELOCITY'] = {}
+         obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['obx']     = np.zeros((nob))
+         obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['oby']     = np.zeros((nob))
+         obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['obz']     = np.zeros((nob))
+         obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['flag']    = np.zeros((nob))  
+         obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['DART_QC'] = np.zeros((nob))
+         for cindex,copy in enumerate(self.copy):
+            #--- Loop Over the Different Copies
+            obs[plat_name]['DOPPLER_RADIAL_VELOCITY'][copy] = np.zeros((nob)) 
+            obcount = 0 
+            for oindex in vr_ob_index:
+               #--- Make sure platform number is the same as radar number
+               if float(test[oindex+plat_index]) == nrdr+1:       
+                  ob_loc = test[oindex+loc_index].split()
+                  obs[plat_name]['DOPPLER_RADIAL_VELOCITY'][copy][obcount]      = float(test[oindex+cindex+1])
+                  if cindex == 0:
+                     obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['obx'][obcount]     = float(ob_loc[0])
+                     obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['oby'][obcount]     = float(ob_loc[1])
+                     obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['obz'][obcount]     = float(ob_loc[2]) 
+                     obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['flag'][obcount]    = float(test[oindex+flag_index])
+                     obs[plat_name]['DOPPLER_RADIAL_VELOCITY']['DART_QC'][obcount] = float(test[oindex+dartqc_index])
+                  obcount += 1
+
+
+      #---- JDL Final Addition - Need to reshape the different dictionary entries to occupy the space of ntilt,ny,nx
+      #---- This could be done in a seperate function
+
+      return obs
+               
 #---Step 6 Create NetCDF file
 #fn = 'radar_obs.nc'
 #wrtfile = Dataset(fn, 'w', format='NETCDF4')
