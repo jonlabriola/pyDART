995eff6e7981a0cf2708969f63d8666a29207dbf
jonlabriola <jonathan.labriola@noaa.gov>
2021-09-14 09:55:27 -0500

    Updating bash scripts that call programes to generate radar/conventional observation classes

---
+++ scripts/run_radars.sh
@@ -0,0 +1,47 @@
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000006.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000007.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000008.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000009.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000009.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000009.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000009.nc &
+wait
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000009.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000010.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000011.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000012.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000013.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000014.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000015.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000016.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000017.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000018.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000019.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000020.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000021.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000023.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000024.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000025.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000026.nc &
+python idealRadObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000027.nc &
+wait
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000009.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000010.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000011.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000012.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000013.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000014.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000015.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000016.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000017.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000018.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000019.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000020.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000021.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000023.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000024.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000025.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000026.nc &
+python idealConvObs.py /scratch/jonathan.labriola/idealized/250m/LES_Injection_Turbulent/all/LES_02/cm1out_rst_000027.nc &
+wait
+
