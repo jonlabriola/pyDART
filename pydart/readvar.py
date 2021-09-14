a03ee4cca27254368f6876c4330c115177ff46b6
jonlabriola <jonathan.labriola@noaa.gov>
2021-09-14 15:21:07 -0500

    Updated the capability to read in either forecast or restart files when generating simulated obs

--- pydart/readvar.py
+++ pydart/readvar.py
@@ -9,28 +9,59 @@ import pkg_resources
 def read_cm1(path,time_str=None):
    """
    Read in model output that can be used to extract simulated observations
+   You will try to read in one of two file types, Restart and Forecast
+
+   Output:
+      model - A dictionary that contains the saved model output
    """
-   #--- Create a dictionary Array of Model Output
-   #varnames = {'output name in model':'desired save name'}
-   varnames = {'ua':'u','va':'v','wa':'w','dbz':'dbz','rho':'rho',
-               'prs':'p','theta':'pt','qv':'qv','xh':'xh','yh':'yh','zh':'zh'}
+
+   try:
+      #--- Restart File
+      varnames = {'ua':'u','va':'v','wa':'w','dbz':'dbz','rho':'rho',
+                  'prs':'p','theta':'pt','qv':'qv','xh':'xh','yh':'yh','zh':'zh'}
+      model = gen_model_output(path,varnames,time_str,rstfile=True)
+      print('Working with a Restart File')
+   except:
+      #--- Forecast File 
+      varnames = {'u':'u','v':'v','w':'w','dbz':'dbz','rho':'rho',
+                  'prs':'p','th':'pt','qv':'qv','xh':'xh','yh':'yh','zh':'zh'}
+      model = gen_model_output(path,varnames,time_str,rstfile=False)
+      print('Working with a Forecast File')
+
+   return model
+
+
+def gen_model_output(path,varnames,time_str,rstfile):
+   """
+   This function is used to read in model output 
+   with the provided variable names.
+
+   The variables are stored in the model dictionary which is passed down
+   to make observations.
+
+   Required input:
+      path     - A string point to the model output file to be opened
+      varname  - A dictionary containing all the variables you want to read
+                 and what they will be refered to in the model dictionary
+      time_str - A string that is used to denote the evaluated time.  If time_str is None
+                 than the variable will be ignored 
+      rstfile  - A Boolean used to determine if you are working with a restart file
+                 if rstfile == false, than you are working with a forecast file
+
+   Output:
+      model - A dictionary that contains the saved model output.
+   """
    model = {}
-   #--- Read In
-   #xmin  =150 #xmax = 200 #ymin = 150 #ymax = 200
-   #xmin = 0
-   #xmax = 300
-   #ymin = 0
-   #ymax = 300
    dumpfile = Dataset(path,"r",fortmat='NETCDF4')
    for var in varnames.keys():
       if var in ['xh','yh','zh']: #--- 1-D variables
          var_tmp = np.squeeze(dumpfile.variables[var][:])
          if var in ['xh']: var_tmp = var_tmp[:]
          elif var in ['yh']: var_tmp = var_tmp[:]
-      elif var in ['ua','va','wa']: #--- Unstagger the grid for certain vars
-         if var in ['ua']: unstag_ax = 2 #--- x-axis
-         if var in ['va']: unstag_ax = 1 #--- y-axis
-         if var in ['wa']: unstag_ax = 0 #--- z-axis
+      elif var in ['ua','va','wa','u','v','w']: #--- Unstagger the grid for certain vars
+         if var in ['ua','u']: unstag_ax = 2 #--- x-axis
+         if var in ['va','v']: unstag_ax = 1 #--- y-axis
+         if var in ['wa','w']: unstag_ax = 0 #--- z-axis
          var_tmp = pydart.interp.unstagger_grid(np.squeeze(dumpfile.variables[var][0,:,:,:]),unstag_ax)
          var_tmp = var_tmp[:,:,:]
       else: #--- No Staggered Grids
@@ -39,9 +70,13 @@ def read_cm1(path,time_str=None):
       model[varnames[var]] = var_tmp
 
    #--- Other Grid Parameters
-   model['dx'] = model['xh'][1] - model['xh'][0]
+   if rstfile:
+     scale_factore =  1
+   else:
+     scale_factor = 1000. #--- Forecast file are written in units of km, want to convert to m
+   model['dx'] = (model['xh'][1] - model['xh'][0]) * scale_factor
    model['nx'] = model['xh'].shape[0]
-   model['dy'] = model['yh'][1]-model['xh'][0]
+   model['dy'] = (model['yh'][1]-model['yh'][0]) * scale_factor
    model['ny'] = model['yh'].shape[0]
    model['nz'] = model['zh'].shape[0]
 
@@ -65,7 +100,8 @@ def read_cm1(path,time_str=None):
 
 
    dumpfile.close()
-   return model
+   return(model)
+
 
 def obcode():
    """
