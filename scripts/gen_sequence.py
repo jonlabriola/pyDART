ea9d6ed67eacf5631a55a04855449c6e947173cc
jonlabriola <jonathan.labriola@noaa.gov>
2021-09-27 20:13:32 -0500

    Updated gen_sequence.py to be more user friendly (i.e., a command prompt allows to specify where the pickle files are located

--- scripts/gen_sequence.py
+++ scripts/gen_sequence.py
@@ -2,14 +2,17 @@ import pickle, numpy, pydart, os, argparse
 import numpy as np
 #--- Convert either observation class to obs_sequence file 
 parser = argparse.ArgumentParser()
+parser.add_argument("file_loc",type=str,default='./',help='The pathway the pickle files are located')
 parser.add_argument("--time",type=str,default='200905152323',help='The last output time (if different from first)')
 parser.add_argument("--conv",action='store_true',help='Work only with conventional obs')
 parser.add_argument("--radar",action='store_true',help='Work only with radar obs')
 parser.add_argument("--outpath",type=str,default='N/A',help='Output path, if desired')
 arguments = parser.parse_args()
 
-conv_class = '/work/jonathan.labriola/python_scripts/pyDART/output/conv_obs_%s.pickle'%arguments.time
-radar_class = '/work/jonathan.labriola/python_scripts/pyDART/output/radar_obs_%s.pickle'%arguments.time
+#conv_class = '/work/jonathan.labriola/python_scripts/pyDART/output/conv_obs_%s.pickle'%arguments.time
+#radar_class = '/work/jonathan.labriola/python_scripts/pyDART/output/radar_obs_%s.pickle'%arguments.time
+conv_class = '%s/conv_obs_%s.pickle'%(arguments.file_loc,arguments.time)
+radar_class = '%s/radar_obs_%s.pickle'%(arguments.file_loc,arguments.time)
 
 if arguments.conv:
    conv = pickle.load(open(conv_class,"rb"))
