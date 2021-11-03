cc9e663138d8f444a759540776f33c492a562791
jonlabriola <jonathan.labriola@noaa.gov>
2021-11-03 08:18:59 -0500

    scripts that are used to evaluate simulations with multiple radars

---
+++ scripts/multi_plat_ens_stats.sh
@@ -0,0 +1,18 @@
+#python updated_ens_stats.py RADAR_REFLECTIVITY --multi_sound  --time 20090516012300   --platform 1 --tilt -1 --reliability --kernel 3.33 --threshold 50  --nplat 2 --nen 10 &
+#python updated_ens_stats.py RADAR_REFLECTIVITY --multi_sound  --time 20090516012300   --platform 1 --tilt -1 --reliability --kernel 3.33 --threshold 50  --nplat 2 --nen 10 --no_prof &
+
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101030000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33 --no_prof &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101030000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33  &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101033000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33 --no_prof &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101033000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33  &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101040000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33 --no_prof &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101040000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33  &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101043000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33  &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101043000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33 --no_prof &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101050000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33  &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101050000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33 --no_prof &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101053000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33  &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101053000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33 --no_prof &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101060000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33  &
+python updated_ens_stats.py RADAR_REFLECTIVITY --coarse --time 20210101060000 --nplat 4 --nen 1 --threshold 40   --limit --tilt -1 --reliability --kernel 3.33 --no_prof &
+wait
---
+++ scripts/multi_plat_perf.sh
@@ -0,0 +1,51 @@
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101030000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101030000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101030000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101030000 --nen 1 --limit_poorcov --no_prof &
+
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101033000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101033000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101033000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101033000 --nen 1 --limit_poorcov --no_prof &
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101040000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101040000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101040000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101040000 --nen 1 --limit_poorcov --no_prof &
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101043000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101043000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101043000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101043000 --nen 1 --limit_poorcov --no_prof &
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101050000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101050000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101050000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101050000 --nen 1 --limit_poorcov --no_prof &
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101053000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101053000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101053000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101053000 --nen 1 --limit_poorcov --no_prof &
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101060000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101060000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101060000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101060000 --nen 1 --limit_poorcov --no_prof &
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101063000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101063000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101063000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101063000 --nen 1 --limit_poorcov --no_prof &
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101070000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101070000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101070000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101070000 --nen 1 --limit_poorcov --no_prof &
+
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --no_prof  --time 20210101073000 --nen 1 &
+#python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101073000 --nen 1 &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101073000 --nen 1 --limit_poorcov &
+python performance_diagram_multi_plat.py RADAR_REFLECTIVITY --nplat 4 --tilt -1 --threshold 45 --kernel 3 --time 20210101073000 --nen 1 --limit_poorcov --no_prof &
+wait
